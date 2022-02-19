f446129f14f6a2d55fddf9a03d0e03fb860d489a
only1finesse <72461240+only1finesse@users.noreply.github.com>
2022-02-18 16:30:25 -0800

    chapters 1 - 12

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -11,9 +11,61 @@
 //       '###'
 //   pyramid(3)
 //       '  #  '
-//       ' ### '
-//       '#####'
+//       '  ###  '
+//       ' ##### '
+//       '#######'
+//   pyramid(5)
+//       '     #     '
+//       '    ###    '
+//       '   #####   '
+//       '  #######  '
+//       ' ######### '
+//       '###########'
 
-function pyramid(n) {}
+function pyramid(n, steps = 0, pyramidStairs = '' ) {
+    // -------- my solution ----------
+
+    // steps = 0
+    // let pyramidBase = ((2 * n) - 1);
+    // let pyramidArray = new Array(pyramidBase).fill(' ');
+
+    // let midpoint = Math.floor((pyramidArray.length / 2))
+
+    // while (steps < n) {
+    //     if (steps === 0) {
+    //         pyramidArray[midpoint] = '#'
+    //     } else {
+    //         pyramidArray[midpoint - steps] = '#'
+    //         pyramidArray[midpoint + steps] = '#'
+    //     }
+    //     steps++
+    //     console.log(pyramidArray.join(''))
+    // }
+
+    // -------- my solution ----------
+
+
+
+    if (steps === n) {
+        return
+    }
+
+    if (pyramidStairs.length === 2*n - 1) {
+        console.log(pyramidStairs)
+        return pyramid(n, steps+1)
+    }
+
+    const midpoint = Math.floor((2*n -1)/2)
+
+
+    if (midpoint - steps <= pyramidStairs.length && midpoint + row >= pyramidStairs.length) {
+        
+    }
+
+
+
+}
+
+pyramid(30)
 
 module.exports = pyramid;
