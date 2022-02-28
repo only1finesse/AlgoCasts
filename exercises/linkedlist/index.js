feaa2293db355732fb41ada97c5838a0b24cd2ff
only1finesse <72461240+only1finesse@users.noreply.github.com>
2022-02-28 14:32:07 -0800

    queues, stacks and linkedlist starter

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,61 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data, next) {
+        this.data = data
+        this.next = next || null
+    }
+}
 
-class LinkedList {}
+
+class LinkedList {
+    constructor() {
+        this.head = null
+    }
+
+    insertFirst(data) {
+        const node = new Node(data, this.head)
+        this.head = node
+    }
+
+    size() {
+        let list = []
+        let node = this.head
+        while (node !== null) {
+            list.push(node.data)
+            node = node.next
+        }
+        return list.length
+    }
+
+    getFirst() {
+        return this.head
+    }
+
+    getLast() {
+        let node = this.head;
+        let lastNode = null
+
+        while (node){
+            lastNode = node
+            node = node.next
+        }
+        return lastNode
+    }
+
+    clear() {
+        this.head = null
+    }
+
+    removeFirst(){
+        let node = this.head; 
+        this.head = node.next
+    }
+    
+    removeLast(){
+        
+    }
+}
 
 module.exports = { Node, LinkedList };
